815a466e3333f450facbb8b339af98fb0d805b4f
astroxii <67867895+astroxii@users.noreply.github.com>
2022-05-21 20:19:18 -0300

    Add actions bar, design improvements

--- todlis/src/Components/Home.js
+++ todlis/src/Components/Home.js
@@ -1,25 +1,27 @@
 import { useState } from "react";
 import "../Styles/Home.css";
+import Actions from "./Subcomponents/Actions";
 import Recent from "./Subcomponents/Recent";
 import Sidenav from "./Subcomponents/Sidenav";
 import Welcome from "./Subcomponents/Welcome";
 
-export default function Home(props)
+export default function Home({user, setUser, view, setView})
 {
-    const [subView, setSubView] = useState(props.view.split(".")[1]);
+    const [subView, setSubView] = useState(view.split(".")[1]);
 
     return(
         <section className="todos-section">
-            <Sidenav user={props.user} setUser={props.setUser} subView={subView} setSubView={setSubView} />
+            <Sidenav user={user} setUser={setUser} subView={subView} setSubView={setSubView} />
             {
                 subView === "Welcome" ?
-                <Welcome user={props.user} setUser={props.setUser} />
+                <Welcome user={user} setUser={setUser} />
                 :
                 subView === "Recent" ?
                 <Recent />
                 :
                 null
             }
+            <Actions user={user} setUser={setUser} />
         </section>
     );
 }
---
+++ todlis/src/Components/Subcomponents/Actions.js
@@ -0,0 +1,8 @@
+export default function Actions({user, setUser})
+{
+    return(
+        <div className="actions-bar">
+            <button type="button" className="action-button">+</button>
+        </div>
+    );
+}
