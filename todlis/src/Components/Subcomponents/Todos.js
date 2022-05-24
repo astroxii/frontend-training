9d6de8304a7d4d1993a486e74244ec8b92228695
astroxii <67867895+astroxii@users.noreply.github.com>
2022-05-24 20:07:38 -0300

    ignore file update

--- todlis/src/Components/Subcomponents/Todos.js
+++ todlis/src/Components/Subcomponents/Todos.js
@@ -7,6 +7,8 @@ export default function Todos({user, setUser, theme})
             <h2 className={`subview-title ${theme}-text`}>Listas de Tarefas</h2>
             <div className="subview-cards">
                 <button type="button" className={`action-card ${theme}-background-c`}>
+                    {/* OPENS A TO-DO LIST EDITOR INSIDE THIS SUBVIEW WITH CANCEL, SAVE BUTTONS, AFTER SAVED, RETURN TO THIS SUBVIEW
+                    WITH THE LIST BELOW THIS DIV! */}
                     <span className={`action-icon ${theme}-text`}>+</span>
                     <h4 className={`action-title ${theme}-text`}>Criar Nova Lista</h4>
                 </button>
